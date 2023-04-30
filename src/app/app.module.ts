76f380df399370fc15b0abc753fec72b72003d08
Leydi Velazco <leydi.velazco@outlook.es>
2023-04-29 22:35:42 -0500

    Add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     GifsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
